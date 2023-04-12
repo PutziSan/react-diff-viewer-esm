19b0f61494b3d783d760c4110e696329e5a30b88
Putzi San <putzisan@putzisan.de>
2023-04-12 08:14:50 +0200

    accept react 17 + 18

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,5 +4,14 @@ export default defineConfig({
   entry: ["src/index.tsx"],
   sourcemap: true,
   format: ["esm", "cjs"],
-  dts: true,
+  outExtension: (ctx) => {
+    switch (ctx.format) {
+      case "cjs":
+        return { js: ".js" };
+      case "esm":
+        return { js: ".module.js" };
+      case "iife":
+        return { js: ".iife.js" };
+    }
+  },
 });
